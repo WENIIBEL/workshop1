e54187ea219e822feb8c9105497e3129377a36e8
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-09-24 14:25:34 -0500

    ejercicio punto 6

--- Empleado.ts
+++ Empleado.ts
@@ -3,15 +3,20 @@ import { Persona } from "./Persona";
 class Empleado extends Persona {
     salario:number
 
-    constructor(salario:number, nombre:string,edad:number) {
+    constructor(salario:number, nombre:string, edad:number) {
         // hace llamado al constructor de la clase principal
         super(nombre,edad)
-        this.salario = salario
-        
+        this.salario = salario  
     }
 
    trabajar(horas:number):string {
-    return `El empleado ha trabajado ${horas} horas al día`
+    return `El empleado ha trabajado ${horas} horas al día` + this.saludar()
    }
+
+   override  saludar(): string {
+       return `Hola ${this.nombre} tu edad es ${this.getEdad} años y tu salario es de ${this.salario}`
+   }
+
+   
 
 }
