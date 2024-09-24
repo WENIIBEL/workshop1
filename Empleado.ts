b0153ef5737c2f9b1fed4d7c919f198c08094391
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-09-24 14:35:36 -0500

    se agrega atributo salario,lo borre sin querer

--- Empleado.ts
+++ Empleado.ts
@@ -3,6 +3,7 @@ import { Persona } from "./Persona";
 class Empleado extends Persona {
     salario:number
 
+    
     constructor(salario:number, nombre:string, edad:number) {
         // hace llamado al constructor de la clase principal
         super(nombre,edad)
