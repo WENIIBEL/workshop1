8599d808f6f003d90b7bb5dd7317b47010f7cfe7
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-09-25 09:46:34 -0500

    ejercicio del punto 7

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Persona } from "./Persona";
+import { Empleado } from "./Empleado";
 
 const personas:Persona [] = [
     new Persona ("wendy",22),
@@ -14,5 +15,18 @@ const personas:Persona [] = [
 ]
 personas.forEach(persona => console.log(persona.saludar()));
 
+const empleados:Empleado [] = [
+    new Empleado (350,"juan",25),
+    new Empleado (250,"Kely",28),
+    new Empleado (150,"Vilet",18),
+    new Empleado (526,"Jhona",36),
+    new Empleado (365,"Fredy",45),
+    new Empleado (785,"Maili",23),
+    new Empleado (265,"Noha",36),
+    new Empleado (456,"Dante",30),
+    new Empleado (457,"Nicol",21),
+    new Empleado (654,"NIcolas",20)
+]
+empleados.forEach(empleado => console.log(empleado.saludar()));
 
 
