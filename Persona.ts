bf1d715894b2ea0b0b2c5677c48cdc23b8f7fe15
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-09-25 10:35:49 -0500

    se modifica por el punto 8

--- Persona.ts
+++ Persona.ts
@@ -1,15 +1,20 @@
+import { Direccion } from "./interfaces";
+
 export class Persona {
+    
+    direccion:Direccion
     nombre:string
     private edad:number
-
+   
     
-    constructor(nombre:string, edad:number) {
+    constructor(nombre:string, edad:number, direccion:Direccion) {
         this.nombre = nombre
         this.edad = edad
+        this.direccion = direccion
     }
 
     public saludar():string {
-        return `Tu nombre es ${this.nombre} y tienes ${this.edad} años`
+        return `Tu nombre es ${this.nombre} y tienes ${this.edad} años` 
     }
 
     public getEdad(): number {
